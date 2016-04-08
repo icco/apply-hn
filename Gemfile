56f6c97ac34ce7254304571d7215f2b1c6bb45bd
Nat Welch <nat@natwelch.com>
2016-04-08 02:28:47 +0000

    yay apis

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.3.0'
 
+gem 'json'
 gem 'nokogiri'
+gem 'oj'
 gem 'typhoeus'
--- scrape.rb
+++ scrape.rb
@@ -6,3 +6,29 @@ require "rubygems" unless defined?(Gem)
 require "bundler/setup"
 Bundler.require(:default)
 
+require "pp"
+
+posts = {}
+
+page = 0
+loop do
+  # https://hn.algolia.com/api/v1/search_by_date?query=%22apply%20hn%22
+  p page
+  params = {
+    query: "apply hn",
+    hitsPerPage: 50,
+    page: page,
+  }
+  request = Typhoeus::Request.new("https://hn.algolia.com/api/v1/search_by_date", method: :get, params: params, headers: {})
+
+  resp = request.run
+  jsn = JSON.parse resp.response_body
+
+  break if jsn["nbPages"] <= page
+
+  jsn["hits"].each do |e|
+    posts[e["objectID"]] = { title: e["title"], text: e["story_text"] }
+  end
+
+  page += 1
+end
